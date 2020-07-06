7bbef0ca2064047397df1b874a64fc78672eee32
TetMAke <silfrimedina@gmail.com>
2020-07-05 20:16:51 -0500

    Merge PC Mesa

--- Códigos/Vectores2D_SoloLearn.py
+++ Códigos/Vectores2D_SoloLearn.py
@@ -1,36 +1,39 @@
-import math,time # Importing mathematics and time modules
+import math
+import time  # Importing mathematics and time modules
 
 
-def sumavec(f1,theta1,f2,theta2):
+def sumavec(f1, theta1, f2, theta2):
     try:
-        #First vector components
+        # First vector components
         theta1 = math.radians(theta1)
         f1x = f1*math.cos(theta1)
         f1y = f1*math.sin(theta1)
         #print("Vector 1 components\nX:{x} Y:{y}".format(x = round(f1x,4),y = round(f1y,4)))
-    
-        #Second vector components
+
+        # Second vector components
         theta2 = math.radians(theta2)
         f2x = f2*math.cos(theta2)
         f2y = f2*math.sin(theta2)
         #print("Vector 2 components\nX:{x} Y:{y}".format(x= round(f2x,4),y =round(f2y,4)))
 
-        #Vector addition
+        # Vector addition
         frx = f1x+f2x
         fry = f1y+f2y
-        Fr = math.sqrt(math.pow(frx,2)+math.pow(fry,2))
-        Fr = round(Fr,4) #Rounding the result to 4 figures 
-        print('Resultant vector components\nX:{x}, Y:{y}'.format(x = round(frx,4), y = round(fry,4)))
+        Fr = math.sqrt(math.pow(frx, 2)+math.pow(fry, 2))
+        Fr = round(Fr, 4)  # Rounding the result to 4 figures
+        print('Resultant vector components\nX:{x}, Y:{y}'.format(x=round(frx, 4), y=round(fry, 4)))
 
-        #Vector angle
+        # Vector angle
         if frx != 0:
-            angle = math.degrees(math.atan(abs(fry)/abs(frx)))  
-            print('Resultant vector angle\nTeta:{x}'.format(x = round(angle,4)))
-        if Fr != 0:     
-            #When frx is greater than zero 
+            angle = math.degrees(math.atan(abs(fry)/abs(frx)))
+            print('Resultant vector angle\nTeta:{x}'.format(x=round(angle, 4)))
+        if Fr != 0:
+            # When frx is greater than zero
             if frx > 0:
                 if fry < 0:
-                    polar = 360 - angle #calculating angle using  absolute values of the components
+                    # calculating angle using  absolute values of the
+                    # components
+                    polar = 360 - angle
                 else:
                     polar = angle
             elif frx < 0:
@@ -46,36 +49,38 @@ def sumavec(f1,theta1,f2,theta2):
         else:
             polar = 0.0
             print('Vectors cancelled each other')
-                
-        
-        if round(polar,4) == 360: #Rounding angles very close to  360 degrees and set it to zero degrees
+        # Rounding angles very close to  360 degrees and set it to zero degrees
+        if round(polar, 4) == 360:
             polar = 0
-            
-        print("Resulting vector\nMagnitude:{x} Polar Angle:{y}".format(x = round(Fr,4),y = round(polar,4)))
-        
+
+        print("Resulting vector\nMagnitude:{x} Polar Angle:{y}".format(
+            x=round(Fr, 4), y=round(polar, 4)))
+
     except TypeError:
         print('Something goes wrong...')
-        time.sleep(2) #Do nothhing for 2 seconds
+        time.sleep(2)  # Do nothhing for 2 seconds
         print('Variable type error')
     return
+
 
 def ingresovalores():
     F1 = 'x'
     t1 = 'x'
     F2 = 'x'
-    t2 = 'x'   
-    #Inputting values
+    t2 = 'x'
+    # Inputting values
     print('Defining 2D vector number 1')
-    while F1 == 'x' and t1 == 'x': 
+    while F1 == 'x' and t1 == 'x':
         try:
-            F1 =float(input('Magnitude:'))
+            F1 = float(input('Magnitude:'))
             if F1 < 0:
                 print('Something goes wrong...')
                 time.sleep(2)
-                raise ValueError #Rasing a Value Error when a negative number is input
-            t1 =float(input('Angle:'))
+                # Rasing a Value Error when a negative number is input
+                raise ValueError
+            t1 = float(input('Angle:'))
 
-        except ValueError: #Used for type errors (wrong type of variables)
+        except ValueError:  # Used for type errors (wrong type of variables)
             print('Something goes wrong...')
             time.sleep(2)
             print('Incorrect value for vector 1')
@@ -84,16 +89,17 @@ def ingresovalores():
             print('Input the correct values for vector 1')
 
     print('Defining 2D vector number 2')
-    while F2 == 'x' and t2 == 'x': 
+    while F2 == 'x' and t2 == 'x':
         try:
-            F2 =float(input('Magnitude:'))
+            F2 = float(input('Magnitude:'))
             if F2 < 0:
                 print('Something goes wrong...')
                 time.sleep(2)
-                raise ValueError #Rasing a Value Error when a negative number is input
-            t2 =float(input('Angle:'))
-            sumavec(F1,t1,F2,t2)
-        except ValueError: #Used for type errors (wrong type of variables)
+                # Rasing a Value Error when a negative number is input
+                raise ValueError
+            t2 = float(input('Angle:'))
+            sumavec(F1, t1, F2, t2)
+        except ValueError:  # Used for type errors (wrong type of variables)
             print('Something goes wrong...')
             time.sleep(2)
             print('Incorrect value for vector 2')
@@ -103,14 +109,13 @@ def ingresovalores():
     return
 
 
+# Variables default values
 
-#Variables default values
-    
-ans = 's' #Default variable 'ans'  value
+ans = 's'  # Default variable 'ans'  value
 ingresovalores()
-while 1:        
+while 1:
     ans = input('Would you like to sum other two vectors? Y/N ')
-    ans = ans.lower() #Turning string to lower case
+    ans = ans.lower()  # Turning string to lower case
     if ans == 'n':
         print('Thanks for using the 2D vector adder')
         break
@@ -118,15 +123,3 @@ while 1:
         ingresovalores()
     else:
         print('Choose a valid option')
-
-        
-
-
-
-    
-    
-
-    
-    
-    
-
