123148d470711390e79bdcc576269d6550d033f4
TetMAke <silfrimedina@gmail.com>
2020-07-09 17:37:11 -0500

    Se añadio la clase de Operaciones matematicas y Operaciones relacionales.

---
+++ Curso Xcript/app-comerciales-kivy/clases/C8-Ingreso_de_datos.py
@@ -0,0 +1,8 @@
+""" Manipulación de datos
+input funcion de toma de datos ingresados por el usuario.
+"""
+login = input("Login:")
+sena = input("Contraseña:")
+
+print("El usuario informado fue: %s, la contraseña digitada fue: %s" %(login, sena))
+
---
+++ Curso Xcript/app-comerciales-kivy/clases/C9-Operaciones_matematicas.py
@@ -0,0 +1,96 @@
+"""Operaciones matemáticas
+Son realizadas a través de los operadores tradicionales,
+que también se encuentran incluidos en el lenguaje.
+"""
+
+# print(10 + 10)
+# print(100 / 10)
+# print(2 * 400)
+#
+# # Suma
+# print(10 + 10)
+# print(10 + (50 + 50))
+#
+# # Resta
+# print(10 - 10)
+# print(1000 - 80)
+#
+# # Division
+# print(10 / 5)
+# print(10 / 6)
+# print(10 // 6)  # Resultado a entero por eliminacion de los decimales.
+#
+# # Multiplicación
+# print(10 * 800)
+# print(55 * 5)
+
+"""
+No Usar {}, [] como separadores de prioridad.
+"""
+
+"""
+Módulo de la División
+
+Es el operador que permite obtener
+el resto de una división.
+"""
+
+# print(6 % 2)
+# print(3 % 2)
+# print(4 % 2)
+# print(5 % 2)
+# print(7 % 3.1)
+
+# print(900 % 100 == 0)
+
+# num1 = float(input("Digite un numero: "))
+# num2 = float(input("Digite otro numero: "))
+#
+# division = num1 / num2
+# resto = num1 % num2
+# print()
+# print(num1, "dividido por", num2, "es igual a:", division)
+# print("El resto de la division entre", num1, "y", num2, "es:", resto)
+#
+"""
+Potenciación y Radicación
+
+Son los operadores matemáticos que permiten
+obtener los resultados correspondientes a
+sus respectivas propiedades.
+Potenciación base**potencia
+Radicación base**(1/potencia)
+
+"""
+
+"""
+Operadores relacionales
+Son utilizados para verificar condiciones
+y juzgar si esta son verdaderas o falsas
+
+De igualdad
+Igualdad: ==
+Diferencia: !=
+Ejemplo:
+(x == y) x es igual a y
+(x !== y) x es diferente de y
+
+Diferenciales
+Mayor a: >
+Menor a: <
+Mayor o igual a: >=
+Menor o igual a: <=
+
+"""
+print(100 == 100)
+print("a" == "a")
+print("a" == "b")
+a = False
+b = True
+print(a == b)
+
+
+print(3>3)
+print(5<5.1)
+print(5<=5.1)
+print((5<5.1)or(5==5.1))
